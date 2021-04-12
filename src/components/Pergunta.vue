8e9f38427083ffcee83cc81f3506d1d7d92ba511
Johnston Ferreira <johnstonferreira99dev@gmail.com>
2021-04-12 20:37:33 -0300

    init
    Co-authored-by: Ricardo Bonino <ricardo@infosiscs.com>
    Co-authored-by: George Cirilo <george-cirilo@users.noreply.github.com>
    Co-authored-by: Ateliê de Ideias <ateliedeideias@users.noreply.github.com>

---
+++ src/components/Pergunta.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <b><span v-html="perguntaAtual.pergunta"></span></b><br />
+
+        <div v-for="(item, indice) in perguntaAtual.respostas" :key="`resposta-${indice}`">
+            <md-button v-html="item" v-on:click="responder(indice)"></md-button><br />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+export default {
+    name: 'Pergunta',
+    props: ['perguntas'],
+    created: function() {
+        console.log('Perguntas', this.perguntas);
+    },
+    computed: {
+        perguntaAtual: function() {
+            return this.perguntas[0];
+        }
+    },
+    methods: {
+        responder(indice) {
+            if (indice === this.perguntaAtual.correta - 1) {
+                alert(this.perguntaAtual.textoCerto ?? "Parabéns, é exatamente isso.");
+            } else {
+                alert(this.perguntaAtual.textoErrado ?? "Humm, Infelizmente você errou.");
+            }
+        }
+    }
+}
+</script>
